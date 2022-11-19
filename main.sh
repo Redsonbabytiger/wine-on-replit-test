c89f87cf87e9a310310247e7e6ab9e27b322e91d
Redsonbabytiger <84416518+Redsonbabytiger@users.noreply.github.com>
2022-11-19 17:47:30 +0000

    Added more exe files to run with wine and also added a minecraft folder.

--- main.sh
+++ main.sh
@@ -1,3 +1,9 @@
 export NIXPKGS_ALLOW_UNFREE=1
 cd /home/runner
 wineboot
+sleep 15
+echo "Make sure to type winecfg in to the shell or console to set the config files to what you want"
+sleep 15
+echo "Have a nice sleep"
+bash minecraftlauncher.sh
+
---
+++ minecraft/Minecraftlauncher.sh
@@ -0,0 +1,2 @@
+java -jar 'minecraft/Salwyrr Minecraft Launcher 4.jar'
+/usr/lib/jvm/java-11-openjdk-amd64/bin/java -Xmx512m -cp /home/runner/.Salwyrr/launcher/launcher.jar fr.salwyrr.launcher.frames.Main --salwyrr salwyrr
